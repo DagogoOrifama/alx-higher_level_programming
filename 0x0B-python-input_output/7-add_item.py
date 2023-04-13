7da9866578d558133f74e128cef8efb5a40de3ea
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-04-13 11:52:08 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,17 @@
 import sys
 import json
 from os import path
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON format"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
+
+def load_from_json_file(filename):
+    """Creates a Python object from a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
 
 # Get arguments from command line
 args = sys.argv[1:]
