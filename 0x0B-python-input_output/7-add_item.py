290698d24ec28cc0e437a7f198b9226645be4a6b
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-04-13 11:47:40 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 import sys
 import json
 from os import path
-from save_to_json_file.py import save_to_json_file
-from load_from_json_file.py import load_from_json_file
+from 5-save_to_json_file.py import save_to_json_file
+from 6-load_from_json_file.py import load_from_json_file
 
 # Get arguments from command line
 args = sys.argv[1:]
