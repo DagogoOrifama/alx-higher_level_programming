47493a227064df1c6699f62f73ced78a6ef4e928
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-04-13 11:52:41 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,17 @@
+#!/usr/bin/python3
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
 import sys
-import json
-from os import path
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON format"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
-
-def load_from_json_file(filename):
-    """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
-        return json.load(f)
-
-# Get arguments from command line
-args = sys.argv[1:]
-
-# Create list if file doesn't exist, otherwise load list from file
-filename = "add_item.json"
-if not path.exists(filename):
-    my_list = []
-else:
-    my_list = load_from_json_file(filename)
-
-# Add arguments to list
-my_list.extend(args)
-
-# Save list to file
-save_to_json_file(my_list, filename)
-  
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
