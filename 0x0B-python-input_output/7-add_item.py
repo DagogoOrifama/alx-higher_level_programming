ce592dc906cb9c8ce933b853789864eee8020ae0
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-04-13 11:26:24 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-import os
+"""Add item script."""
 import sys
-import json
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+try:
+    lst = load_from_json_file("add_item.json")
+except:
+    lst = []
 
-filename = 'add_item.json'
-args = len(sys.argv)
+argc = len(sys.argv)
 
-if not os.path.isfile(filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write('[]')
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
 
-if args > 1:
-    data = load_from_json_file(filename)
-    for i in range(1, args):
-        data.append(sys.argv[i])
-    save_to_json_file(data, filename)
+save_to_json_file(lst, "add_item.json")
