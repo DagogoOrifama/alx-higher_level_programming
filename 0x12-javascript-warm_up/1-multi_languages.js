e212d5cc127874ef596fe10d976e9994fcd4c6c5
Dagogo Orifama <dagoris2010@gmail.com>
2023-06-14 20:19:43 +0000

    added files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Script that print 3 line
+
+const tsi = 'C is fun';
+const bek = 'Python is cool';
+const ete = 'JavaScript is amazing';
+console.log(tsi);
+console.log(bek);
+console.log(ete);
