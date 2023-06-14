78ec828375510321cde16b59b3b1b574ee7ffc80
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-06-14 21:34:25 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
-
-function computeFactorial(num) {
-  if (isNaN(num)) {
-    return 1;
-  } else if (num === 0) {
-    return 1;
-  } else {
-    return num * computeFactorial(num - 1);
-  }
+function factorial (num) {
+  const x = Number(num);
+  if (isNaN(x) || x === 1 || x === 0) return (1);
+  return (x * factorial(x - 1));
 }
 
-const num = parseInt(process.argv[2]); // Get the integer argument from command line
-
-console.log(computeFactorial(num));
-
+console.log(factorial(process.argv[2]));
