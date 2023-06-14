706b8e121a0b2b63ea2a4abde42e3c76a6c7d226
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-06-14 21:51:20 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data.js');
+
 const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (newDict[occurrences]) {
+    newDict[occurrences].push(userId);
+  } else {
+    newDict[occurrences] = [userId];
   }
-  newDict[dict[key]].push(key);
 }
+
 console.log(newDict);
