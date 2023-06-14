4222f74b5de9a2b4179678853f8deb165d24943d
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-06-14 21:48:44 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js');
-const newList = list.map((value, index) => value * index);
-console.log('Initial List:', list);
-console.log('New List:', newList);
+let i = 0;
+const list = require('./100-data').list;
+const newList = list.map(x => x * i++);
+console.log(list);
+console.log(newList);
