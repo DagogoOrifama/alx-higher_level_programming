99fba62a5c190d480c54bc93a4414304f73f46a5
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-06-14 21:55:20 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,35 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs');
-
-const sourceFile1 = process.argv[2]; // File path of the first source file
-const sourceFile2 = process.argv[3]; // File path of the second source file
-const destinationFile = process.argv[4]; // File path of the destination file
-
-// Read the contents of the first source file
-fs.readFile(sourceFile1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error('Error reading the first source file:', err);
-    return;
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
-
-  // Read the contents of the second source file
-  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error('Error reading the second source file:', err);
-      return;
-    }
-
-    // Concatenate the contents of the two source files
-    const concatenatedData = data1 + data2;
-
-    // Write the concatenated data to the destination file
-    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
-      if (err) {
-        console.error('Error writing to the destination file:', err);
-        return;
-      }
-
-      console.log('Files concatenated successfully.');
-    });
-  });
-});
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
