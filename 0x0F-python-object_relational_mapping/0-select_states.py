95efb07ad027bf221b76c1efc38a5bf2c7c2bd0c
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2023-06-24 22:42:23 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa
+"""Script displays states in database by ascending id order
+   Script should take 3 arguments:
+   mysql username, mysql password and database name
 """
 
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-import MySQLdb
-from sys import argv
-
-if __name__ == "__main__":
-
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-
-    # create cursor to exec queries using SQL
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cursor.fetchall():
-        print(row)
-    cursor.close()
-    db.close()
+    if len(sys.argv) > 3:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
